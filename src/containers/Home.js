c9d558486c964b7cea99dc9511ea17283bb0fbb2
Fernando <fernandodlv32@gmail.com>
2019-02-04 23:55:48 -0500

    finished home intro animation. added buttons and button animation

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { TweenMax, TimelineMax, Power1 } from 'gsap';
-import { Link } from 'react-router-dom';
-import FLogo from '../res/f_logo.png'
-import { ReactComponent as TestLogo} from '../res/f_logo.svg';
+import { TweenMax, TimelineMax, Power1, Bounce, Elastic, Power4 } from 'gsap';
+
 import * as actions from '../redux/actions/nav.actions';
 import '../styles/home.css';
 import AnimateHOC from '../hocs/Animate';
-import { debounce } from '../utility/utility';
 
 class Home extends Component {
   constructor(props) {
@@ -24,26 +21,52 @@ class Home extends Component {
   componentDidMount = () => {   
     const tl = new TimelineMax();
     tl.to('tl-delay', .3, {opacity: 0})
-      .to('.home-searching', .4, {opacity: 1})
-      .staggerTo('.home-slide', .4, {left: 0, opacity: 1}, .4)
-      // .from('.span-e', .4, {left: 100, top: 0})
-      // .from('.span-e2', .4, {left: 127, top: 0}, '-=.4')
-      
+      .staggerTo('.home-slide', .4, {left: 0, opacity: 1}, .6)
+      .to('.home-span', 0, {opacity: 1})
+      .to('.home-m-span', 0, {opacity: 1})
+      .to('.home-slide', .4, {opacity:0}) 
+      .to('.home-e-span', .4, {top: 0, left: 57})
+      .to('.home-e2-span', .4, {top: 0, left: 91}, '-=.4')
+      .to('.home-t-span', .4, {top: 0, left: 122}, '-=.4')
+      .to('.home-slide', 0, {display: 'none'})
+      .to('.home-fernando', .4, {opacity: 1})
+  }
+
+  enterButton = (type) => {
+    TweenMax.to('.home-button-projects', .5, {color: 'black'})
+    TweenMax.to(`.home-button-bg-${type}`, 1.8, type==='projects'? {top: -50, left: 0, ease: Elastic.easeOut} : {})
+  }
+  leaveButton = (type) => {
+    TweenMax.to('.home-button-projects', .5, {color: '#BCDEFA'})
+    TweenMax.to(`.home-button-bg-${type}`, .5, type==='projects'? {left: -120, top: 30, ease: Power4.easeOut} : {})  
   }
 
   render = () => {
     const { state } = this;
     return (
       <main className={'Home'}>
-        <section className="home-intro-section">
-          <h2 className="home-h2 home-searching">Searching for a</h2>
-          <h2 className="home-h2 home-slide home-motivated"><span className="home-span span-m">m</span>otivated</h2>
-          <h2 className="home-h2 home-slide home-eager"><span className="home-span span-e">e</span>fficient</h2>
-          <h2 className="home-h2 home-slide home-efficient"><span className="home-span span-e2">e</span>nthusiastic</h2>
-          <h2 className="home-h2 home-slide home-thinker"><span className="home-span span-t">t</span>hinker?</h2>
-          <h2 className="home-h2 home-meet">meet</h2>
-          <h2 className="home-h2 home-fernando">Fernando.</h2>
-        </section>
+        <article className="home-intro-article">
+          <header className="home-header">
+            <h2 className="home-h2 home-slide home-motivated">Motivated</h2>
+            <h2 className="home-h2 home-slide home-efficient">efficient</h2>
+            <h2 className="home-h2 home-slide home-enthusiastic">enthusiastic</h2>
+            <h2 className="home-h2 home-slide home-thinker">thinker.</h2>
+            <h2 className="home-h2 home-meet">
+              <span className="home-m-span">M</span>
+              <span className="home-span home-e-span">e</span>
+              <span className="home-span home-e2-span">e</span>
+              <span className="home-span home-t-span">t</span>
+            </h2>
+            <h2 className="home-h2 home-fernando">Fernando.</h2>
+          </header>
+          <section className="home-button-container">
+            <button onMouseLeave={()=>{this.leaveButton('projects')}} onMouseEnter={()=>{this.enterButton('projects')}} className="home-button home-button-projects">
+              <p style={{position:'relative', zIndex:5}}>View Projects</p>
+              <span className="home-button-bg home-button-bg-projects"/>
+            </button>
+            <button className="home-button">Download Resume</button>
+          </section>
+        </article>
       </main>
     )
   }
